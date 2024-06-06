693bc35d6de6b17fee811d6acd34b13fc7e01b06
Aradhya <aradhya@jschamps.com>
2024-06-06 19:34:21 +0530

    create invoice ui

--- src/components/salonappointment/pastappointment/PastAppointment.jsx
+++ src/components/salonappointment/pastappointment/PastAppointment.jsx
@@ -1,19 +1,18 @@
 import { Row, Col } from "react-bootstrap";
-import { Link } from 'react-router-dom';
-import styles from "../upcomingappointment/UpComing.module.css";
-import { useEffect, useState } from 'react';
+import { FiDownload } from "react-icons/fi";
+import { useEffect, useState } from "react";
 import { completedAppointments } from "../../../api/appointments.api";
 import Session from "../../../service/session";
 import Notify from "../../../utils/notify";
-import Popup from "../appointmentdetails/Popup";
+import styles from "../upcomingappointment/UpComing.module.css";
+import ViewDetails from "./ViewDetails";
 
 const PastAppointment = () => {
   const [selectedAppointmentId, setSelectedAppointmentId] = useState(null);
   const [completed, setCompleted] = useState([]);
-  const [showPopup, setShowPopup] = useState(false);
-  const salonId = Session.get('salonId');
+  const [showDetails, setShowDetails] = useState(false);
+  const salonId = Session.get("salonId");
 
- 
   useEffect(() => {
     const fetchAppointments = async () => {
       try {
@@ -30,14 +29,15 @@ const PastAppointment = () => {
 
   const handleViewDetails = (appointmentId) => {
     setSelectedAppointmentId(appointmentId);
-    setShowPopup(true);
+    setShowDetails(true);
   };
-  if(!completed || completed.length===0){
-    return <div className={styles.noContent}>
-      No completed appointments
-    </div>
-   }
-   const getStatusClass = (status) => {
+
+  const handleCloseDetails = () => {
+    setShowDetails(false);
+    setSelectedAppointmentId(null);
+  };
+
+  const getStatusClass = (status) => {
     switch (status) {
       case "PENDING":
         return styles.pending;
@@ -49,7 +49,7 @@ const PastAppointment = () => {
         return styles.completed;
       case "CANCELLED":
         return styles.cancelled;
-        case "CONFIRMED":
+      case "CONFIRMED":
         return styles.confirmed;
       case "IN_SERVICE":
         return styles.inService;
@@ -59,12 +59,16 @@ const PastAppointment = () => {
         return "";
     }
   };
-  
+
+  if (!completed || completed.length === 0) {
+    return <div className={styles.noContent}>No completed appointments</div>;
+  }
+
   return (
     <div>
       <Row className={styles.today}>
         {completed?.map((appointment, index) => (
-          <Col md={6} sm={6} lg={4} xs={12} key={index}>
+          <Col md={6} sm={6} lg={6} xs={12} key={index}>
             <Row className={`${styles.mainDiv} mb-2`}>
               <div className={styles.userInfo}>
                 <Col md={4}>
@@ -82,33 +86,58 @@ const PastAppointment = () => {
                       {`${appointment.user.firstName} ${appointment.user.lastName}`}
                     </span>
                     <br />
-                    <span >{appointment.service}</span>
+                    <span>{appointment.service}</span>
                     <br />
-                    <span className={styles.appointTime}>{appointment.startTime} &nbsp;<span className={styles.gender}>{appointment.serviceType}</span></span>
+                    <span className={styles.appointTime}>
+                      {appointment.startTime} &nbsp;
+                      <span className={styles.gender}>
+                        {appointment.serviceType}
+                      </span>
+                    </span>
                     <br />
-                    <span className={styles.locationDistance}>{appointment.date}</span>
+                    <span className={styles.locationDistance}>
+                      {appointment.date}
+                    </span>
                   </p>
                 </Col>
                 <Col md={4}>
-                <p className={`${styles.status} ${getStatusClass(appointment?.status)}`}>                    {appointment.status}
-                    <br />
-                    <Link onClick={() => handleViewDetails(appointment.id)}>View Details</Link>
+                  <p
+                    className={`${styles.status} ${getStatusClass(
+                      appointment?.status
+                    )}`}
+                  >
+                    {" "}
+                    {appointment.status}
                   </p>
                 </Col>
               </div>
+              <Col className={styles.invoice}>
+                <div className={styles.invoiceDiv}>
+                  <p
+                    onClick={() => handleViewDetails(appointment.id)}
+                    className={styles.viewDetailsLink}
+                  >
+                    View Details{" "}
+                  </p>
+                  <div className={styles.iconDiv}>
+                    <FiDownload className={styles.loadIcon} />
+                    <p>Download Invoice</p>
+                  </div>
+                </div>
+              </Col>
             </Row>
           </Col>
         ))}
       </Row>
-      {showPopup && (
-        <Popup
-          data={selectedAppointmentId}
-          show={showPopup}
-          onHide={() => setShowPopup(false)}
+      {showDetails && (
+        <ViewDetails
+          isOpen={showDetails}
+          onClose={handleCloseDetails}
+          appointmentId={selectedAppointmentId}
         />
       )}
     </div>
   );
-}
+};
 
 export default PastAppointment;
